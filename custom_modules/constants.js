56ff34cea81af93c117c0afc29c7351efc8e0258
root <root@centos64.(none)>
2014-10-19 20:47:44 -0400

    need api for login. new url for answers

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -1,9 +1,10 @@
 module.exports = {
     URLS: {
-    login: 'https://www.okcupid.com/login',
+    login: 'https://www.okcupid.com/login?okc_api=1',
     rate: 'http://www.okcupid.com/quickmatch',
     visit_user: 'http://www.okcupid.com/profile/{username}',
     user_profile: 'http://www.okcupid.com/profile/{username}?okc_api=1',
+    user_answers: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&n=10&low={offset}',
     get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
     quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1'
     },
