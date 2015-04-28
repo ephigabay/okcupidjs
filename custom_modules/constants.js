4b012395e13f75e1ac01fccc578a9c42d8495ecc
Ephi Gabay <gabay.ephi@gmail.com>
2015-04-28 23:32:15 +0300

    made getConversations method return all conversations

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -7,7 +7,8 @@ module.exports = {
         user_answers: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&n=10&low={offset}',
         get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
         quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
-        messages: 'http://www.okcupid.com/messages?okc_api=1'
+        conversations: 'http://www.okcupid.com/messages?low={offset}&okc_api=1',
+        conversation: 'http://www.okcupid.com/messages?readmsg=true&threadid={conversation}&okc_api=1'
     },
     HEADERS: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
 }
