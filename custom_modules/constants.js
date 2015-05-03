332526a261df27db30a5d6a59eab2cf3cbdfe696
user <mail>
2015-05-03 22:26:46 +0300

    everything works with callbacks

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -9,7 +9,8 @@ module.exports = {
         quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
         conversations: 'http://www.okcupid.com/messages?low={offset}&okc_api=1',
         conversation: 'http://www.okcupid.com/messages?readmsg=true&threadid={conversation}&okc_api=1',
-        sendMessage: 'http://www.okcupid.com/apitun/messages/send?&access_token={access_token}'
+        sendMessage: 'http://www.okcupid.com/apitun/messages/send?&access_token={access_token}',
+        deleteConversation: 'http://www.okcupid.com/apitun/messages/{conversation}?&access_token={access_token}&_method=DELETE'
     },
     HEADERS: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
 }
